3cf3874be1a2cce2ec49830d62672b7fbacd39de
prashanth-devadas <prashanth.devadas@yahoo.com>
2019-10-23 21:12:08 -0400

    added javascript files which will handle different areas of the ui

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return <input onChange={this.onInputChange} />
+    }
+
+    onInputChange(event) {
+        
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY='AIzaSyDH5V8jCLjKEtMlgsTPpqs5caCxriuUdZ8';
 
 //Create a new component that outputs HTML code on the DOM 
 
 const App = () => {
-    return <div>Hello!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
