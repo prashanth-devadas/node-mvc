7b13e8c58c62745c37c8e083e55b4d1061a46820
prashanth-devadas <prashanth.devadas@yahoo.com>
2019-11-26 03:27:31 -0500

    hiding api key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
-const API_KEY='AIzaSyDH5V8jCLjKEtMlgsTPpqs5caCxriuUdZ8';
+const API_KEY='oiwjeofjioejfojweofijweojwefijoweij';
 
 //Create a new component that outputs HTML code on the DOM 
 
