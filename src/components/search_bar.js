587ae43a167bff0c8047ca3160c58fb1b2ea1f4a
prashanth-devadas <prashanth.devadas@yahoo.com>
2019-11-28 02:38:30 -0500

    search bar state manipulation

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: ''};
+    }
     render() {
-        return <input onChange={(event) => {console.log(event.target.value)}} />
+        return (
+            <div>
+                <input onChange={(event) => this.setState({term: event.target.value}) }/>
+            </div>
+        );
     }
 }
 
